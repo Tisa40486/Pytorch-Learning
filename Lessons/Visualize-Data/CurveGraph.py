dbbbc7682f7d105f983a97f6a131ef5f25f8f5c2
Mattis <lefrancadammattis@gmail.com>
2026-05-13 00:19:52 +0200

    curve done.

--- Lessons/Visualize-Data/CurveGraph.py
+++ Lessons/Visualize-Data/CurveGraph.py
@@ -2,14 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Data
-x = np.linspace(0, 10, 48) # 50 points between 0 and 10
+x = np.linspace(-1, 30, 200) # 200 points between -1 and 30
 y = np.sin(x) # Sinusoidal curve foreach x value
 
 # Create the curve graph
 fig, ax = plt.subplots(figsize=(10, 6)) # fig = window, ax = graph area, figsize in inches (width, height)
 
 # Draw the curve
-ax.plot(x, y, label='Sin(x)', color='blue', marker='o', markersize=5, markeredgecolor='red', markerfacecolor='red', linewidth=7) # marker='o' adds points on the curve, markersize sets their size
+ax.plot(x,
+        y,
+        label='Sin(x)', 
+        color='blue',
+        marker='o',
+        markersize=5,
+        markeredgecolor='red',
+        markerfacecolor='red',
+        linewidth=7) # marker='o' adds points on the curve, markersize sets their size
 
 # Customize the graph
 ax.set_title('Curve Graph of Sin(x)', fontsize=16)
